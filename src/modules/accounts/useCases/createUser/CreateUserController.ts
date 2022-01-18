b31cd4ff4e22bc96fd14b96622352373958e5cbd
FlpSlva <felipinhoznoroeste@gmail.com>
2022-01-17 23:30:53 -0300

    create user useCases

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,21 @@
+import { container } from "tsyringe"
+import { CreateUserUseCase } from "./CreateUserUseCase"
+import {Response, Request} from 'express'
+
+class CreateUserController{
+
+    async handle(req: Request, res: Response): Promise<Response> {
+
+        const {driver_license, email, name, password, username} = req.body
+
+        const createUserUseCase = container.resolve(CreateUserUseCase)
+
+        await createUserUseCase.execute({driver_license, email, name, password, username})
+
+        return res.status(201).json();
+
+    }
+
+}
+
+export {CreateUserController}
