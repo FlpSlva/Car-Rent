52be59de197fc28c1be765e33aeed1e8ac855c7e
FlpSlva <felipinhoznoroeste@gmail.com>
2022-01-24 13:46:06 -0300

    adding route.authentication.user to index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { categoriesRoutes } from "./cars/categories.routes";
 import { specificationsRoutes } from "./cars/specifications.Routes";
 import { usersRoutes } from "./accounts/users.routes";
+import { authenticateRoutes } from "./accounts/authenticate.routes";
 
 const router = Router();
 
@@ -12,6 +13,6 @@ router.get("/", (req, res) => {
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
-
+router.use(authenticateRoutes)
 
 export {router};
