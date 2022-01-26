9984c498c81a81061b98aedd0a8b6785ec11502f
FlpSlva <felipinhoznoroeste@gmail.com>
2022-01-26 16:13:52 -0300

    fix: throw Error/AppError

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -30,7 +30,7 @@ export async function ensureAuthenticated(req: Request, res: Response, next: Nex
 
         next();
     } catch {
-        throw new Error("Invalid token!")
+        throw new AppError("Invalid token!", 401)
     }
 
 
