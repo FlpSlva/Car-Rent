f9c068138d4ca7ebe6191b4a0b636d7881c1fb6a
FlpSlva <felipinhoznoroeste@gmail.com>
2022-01-26 16:30:31 -0300

    feat: added error middleware

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,19 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            message: err.message
+        })
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: `Internal Server Error - ${err.message}`
+    })
+})
+
 
 
 app.listen(port, () => console.log('server is running on port', port));
